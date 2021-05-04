1d14273db0da6736292790617a51f708489809ae
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-05-04 21:11:53 +0200

    solve some problems in links

---
+++ JS/app.js
@@ -0,0 +1,11 @@
+const user = 'yehiakhaled922',
+    domain = 'gmail.com',
+    element = document.getElementById('email');
+
+// element.innerHTML = `<a href="mailto: '${user} @ ${domain}'">Email</a>`;
+element.addEventListener("click",()=>{
+    element.innerHTML=`<a href="mailto: '${user} @ ${domain}'">Email</a>`
+})
+//OR
+//'<a href="mailto:' + user + '@' + domain + '">Email</a>'
+// const email_link =document.getElementsByClassName("email")
