4762aca35d23c33d5ce777c3b77fc7a96233491f
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-05-06 03:32:26 +0200

    delete counter ,didn't work properly

--- JS/app.js
+++ JS/app.js
@@ -11,37 +11,5 @@ element.addEventListener("click",()=>{
 // const email_link =document.getElementsByClassName("email")
 
 /*TODO counter visitors*/
-incrementAndShowValue();
 
-function incrementAndShowValue() {
-    var value = getCookie("visitcounter") || 0;
-    var newValue = ("00000" + (Number(value) + 1)).slice(-6);
-    var container = document.getElementById("counterVisitor");
-    String(newValue).split("").forEach(function(item, index) {
-        container.children[index].innerHTML = item;
-    });
-    counter++;
-    setCookie("visitcounter", counter);
-}
-
-function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        const date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + value + expires + "; path=https://stacksnippets.net/js";
-}
-
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (    c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
 
